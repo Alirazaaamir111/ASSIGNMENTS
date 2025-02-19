99f0830be9f6cf84e51a21e4edd5a50ca7fd0238
Alirazaaamir111 <alirazaaamir246@gmail.com>
2025-02-18 21:46:22 -0800

    first commit

---
+++ Assigment-01/app.js
@@ -0,0 +1,69 @@
+var username;
+
+var myName = "Ali Raza\n"
+document.write(myName)
+
+
+var message;
+var message = 'Hello World'
+alert(message)
+
+var namme = 'Name ==> Ali Raza'
+var roll = ' Roll Number ==> 321'
+var school = "School ==>SMIT"
+var age = "Age == > 19 Years"
+alert(namme)
+alert(roll)
+alert(school)
+alert(age)
+
+var piz = 'Pizza \nPizz \nPiz \nPi \nP'
+alert(piz)
+
+var email = " \nyour email address is abc@gmail.com"
+document.write(email)
+alert(email)
+
+var book = "A smarter way to learn JavaScript"
+alert(book)
+
+document.write("Yay! I can write HTML content through JavaScript")
+
+//VARIABLES FOR NUMBERS
+
+
+
+
+var age = "I am 18 Year old"
+alert (age)
+
+
+
+//Q:2
+
+
+var birthYear = "My Birth year 1996 "+"Data type of my declared variable is number"
+document.write(birthYear)
+
+
+
+
+
+//Q:3
+
+var Visitortitle = "John Doe"
+var Producttitile = "5 T-shirt(s)"
+var Quantity     ="on XYZ Clothing store"
+
+document.write ( Visitortitle,Producttitile,Quantity)
+
+
+
+
+//
+
+var noOfChilds = 'You Have 3 Childrens.'
+var partnerName = 'Your Partner Name Is abc.'
+var location = "Your Location Is SMIT."
+var job = "You Are Unemployed."
+alert(noOfChilds,partnerName,location,job)

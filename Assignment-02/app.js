9839bedd356911b014815fb38040d1408f87c115
Alirazaaamir111 <alirazaaamir246@gmail.com>
2025-02-20 20:34:36 -0800

    initial commit

---
+++ Assignment-02/app.js
@@ -0,0 +1,53 @@
+// Question 1
+
+
+// var user=+prompt("enter divisible number")
+// if(user % 3 === 0){
+//     alert(${user} is  divisible by 3)
+
+// }
+// else{
+//     alert(${user}is not  divisible by 3) 
+// }
+
+// Question 2
+
+
+// var userIput = +prompt("Enter a numble") 
+// if(userIput % 2===0){
+//     alert(${userIput} is even number)
+// }
+// else{
+//     alert(${userIput} is not even number)
+// }
+
+// Question 3
+// var age = +prompt("Enter Your Age")
+// if(age >30){
+//     alert("old enough")
+// }else{
+//     alert("Too Young")
+// }
+
+
+// Question 3
+
+// ?
+
+// Question 4
+// const char = prompt("Enter a character:");
+
+// if (!isNaN(char)) {
+//     console.log("Input is a number");
+// } else {
+//     const ascii = char.charCodeAt(0);
+//     if (ascii >= 65 && ascii <= 90) {
+//         console.log("Input is an uppercase letter");
+//     } else if (ascii >= 97 && ascii <= 122) {
+//         console.log("Input is a lowercase letter");
+//     } else {
+//         console.log("Input is neither a number nor a letter");
+//     }
+// }
+
+// Question 5
